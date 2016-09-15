42203c1df16d723b3c0e4a66ebc3331de4ea5b16
Zhao Wei Liew <zhaoweiliew@gmail.com>
2016-09-15 18:10:59 +0800

    Telecom: Break qti-telephony-framework dependency
      Not all devices have qti-telephony-framework,
      so fall back to AOSP's methods if needed.
    
    Change-Id: Ib6de738a292203be3d125ab34d6c8682a32007d5

--- src/com/android/server/telecom/NewOutgoingCallIntentBroadcaster.java
+++ src/com/android/server/telecom/NewOutgoingCallIntentBroadcaster.java
@@ -123,7 +123,7 @@ public class NewOutgoingCallIntentBroadcaster {
                                 getResultExtras(false), disconnectTimeout);
                         endEarly = true;
                     } else if (TelephonyUtil.isPotentialLocalEmergencyNumber(
-                            resultNumber)) {
+                            mPhoneNumberUtilsAdapter, mContext, resultNumber)) {
                         Log.w(this, "Cannot modify outgoing call to emergency number %s.",
                                 resultNumber);
                         disconnectTimeout = 0;
@@ -449,7 +449,7 @@ public class NewOutgoingCallIntentBroadcaster {
      */
     private boolean isPotentialEmergencyNumber(String number) {
         Log.v(this, "Checking restrictions for number : %s", Log.pii(number));
-        return (number != null) && TelephonyUtil.isPotentialLocalEmergencyNumber(number);
+        return (number != null) && TelephonyUtil.isPotentialLocalEmergencyNumber(mPhoneNumberUtilsAdapter, mContext, number);
     }
 
     /**
